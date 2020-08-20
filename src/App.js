a694cfdb9b30adbb8400da6b368a6a7e160e27c1
Tim Hettler <hettler@work.co>
2020-08-20 10:51:54 -0400

    baby match!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const BABIES = [
+  {
+    name: "Allie",
+    src: require("./assets/allie.jpeg"),
+  },
+  {
+    name: "Cherrie",
+    src: require("./assets/cherrie.jpg"),
+  },
+  {
+    name: "Cheryl",
+    src: require("./assets/cheryl.jpg"),
+  },
+  {
+    name: "Chris",
+    src: require("./assets/chris.jpg"),
+  },
+  {
+    name: "Dan",
+    src: require("./assets/dan.jpg"),
+  },
+  {
+    name: "Elizabeth",
+    src: require("./assets/elizabeth.jpeg"),
+  },
+  {
+    name: "Ivy",
+    src: require("./assets/ivy.jpg"),
+  },
+  {
+    name: "Jim",
+    src: require("./assets/jim.jpg"),
+  },
+  {
+    name: "Lauren",
+    src: require("./assets/lauren.jpeg"),
+  },
+  {
+    name: "Leon",
+    src: require("./assets/leon.jpeg"),
+  },
+  {
+    name: "Lynnie",
+    src: require("./assets/lynnie.jpg"),
+  },
+  {
+    name: "Mary Hettler",
+    src: require("./assets/maryb.jpg"),
+  },
+  {
+    name: "Mary Young",
+    src: require("./assets/maryh.jpeg"),
+  },
+  {
+    name: "Mike",
+    src: require("./assets/mike.jpg"),
+  },
+  {
+    name: "Paul",
+    src: require("./assets/paul.jpg"),
+  },
+  {
+    name: "Rebecca",
+    src: require("./assets/rebecca.jpg"),
+  },
+  {
+    name: "Tim",
+    src: require("./assets/tim.jpg"),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid">
+        {BABIES.map(({ name, src }) => (
+          <figure>
+            <img src={src} alt="" />
+          </figure>
+        ))}
+      </div>
     </div>
   );
 }
